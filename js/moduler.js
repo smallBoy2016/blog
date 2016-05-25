56f24ca38ecc207e5d550434591e385d57239df4
smallBoy2016 <f224031@126.com>
2016-05-25 22:52:34 +0800

    moduler5-25

--- js/moduler.js
+++ js/moduler.js
@@ -7,3 +7,20 @@
 
 // 1.自动轮播函数
 
+
+
+/*changeTag : 标签选项卡
+arrHead : 切换的标题
+arrContent ： 切换标题对应的显示内容。
+*/
+function changeTag(arrHead,arrContent){
+	for(var i=0;i<arrHead.length;i++){
+		arrHead[i].index = i;
+		arrHead[i].onclick = function(){
+			for(var i=0;i<arrContent.length;i++){
+				arrContent[i].style.display = 'none';
+			}
+			arrContent[this.index].style.display = 'block';
+		};
+	}
+}
